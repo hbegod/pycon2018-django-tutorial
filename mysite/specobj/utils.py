1d8d0eda93aef2db87024d15d14af9a24f43f7cf
Roman Kolesnikov <hbegod@mail.ru>
2018-12-24 01:30:45 +1000

    add to utils

--- mysite/specobj/utils.py
+++ mysite/specobj/utils.py
@@ -1,3 +1,29 @@
+from .models import Obj, Spec
+
+
+
+def get_object_specs(object):
+    #  Person.object - менеджер модели
+    #  qs_person - это QuerySet
+    qs_specs = Obj.
+
+
+    
+    qs_person = Person.objects.filter(last_name__contains=surname)
+    try:
+        #  person - инстанс модели Рerson.
+        person =  qs_person.get(first_name__contains=name)
+        # обработка отсутствия записи в базе
+    except Person.DoesNotExist as e:
+        raise VseSlomalosExeption("Это кто вообще?")
+    except Person.MultipleObjectsReturned as e:
+        raise VseSlomalosExeption("А что их несколько?")
+    return
+
+
+
+
+
 # TODO Для существующей моделей, необходимо написать функции
 # TODO (объект и характеристика, это модели в аппке):
 # TODO
